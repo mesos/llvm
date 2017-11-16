02e252f3421b962d0741bad37883c908762ebdca
Michael Park <mcypark@gmail.com>
2017-11-16 07:08:40 -0800

    Replace `tar` with `7z`.

--- mesos-llvm.ps1
+++ mesos-llvm.ps1
@@ -1,10 +1,13 @@
 $VERSION="2017-11-15"
 $PREFIX="mesos-llvm/${VERSION}"
 
-mkdir llvm
-wget -O - https://releases.llvm.org/5.0.0/llvm-5.0.0.src.tar.xz | tar --strip-components=1 -xJ -C llvm
-git clone --depth 1 -b mesos_50 https://github.com/mesos/clang.git llvm/tools/clang
-git clone --depth 1 -b mesos_50 https://github.com/mesos/clang-tools-extra.git llvm/tools/clang/tools/extra
+wget https://releases.llvm.org/5.0.0/llvm-5.0.0.src.tar.xz -OutFile llvm-5.0.0.src.tar.xz
+7z e llvm-5.0.0.src.tar.xz
+7z x llvm-5.0.0.src.tar
+mv llvm-5.0.0.src llvm
+
+git clone -q --depth 1 -b mesos_50 https://github.com/mesos/clang.git llvm/tools/clang
+git clone -q --depth 1 -b mesos_50 https://github.com/mesos/clang-tools-extra.git llvm/tools/clang/tools/extra
 
 cmake -Thost=x64 -DCMAKE_INSTALL_PREFIX="${PREFIX}" llvm
 
