f63a8467fd9f75130eb6152539e0bfc7966ae414
Michael Park <mcypark@gmail.com>
2018-03-15 14:26:24 -0700

    Updated the rest to upto date for 6.0.

--- mesos-format.ps1
+++ mesos-format.ps1
@@ -1,13 +1,13 @@
 $TOOL = "mesos-format"
-$VERSION = "2017-11-17"
+$VERSION = "2018-03-08"
 $PREFIX = "${TOOL}/${VERSION}"
 
-wget https://releases.llvm.org/5.0.0/llvm-5.0.0.src.tar.xz -OutFile llvm-5.0.0.src.tar.xz
-7z e llvm-5.0.0.src.tar.xz
-7z x llvm-5.0.0.src.tar
-mv llvm-5.0.0.src llvm
+wget https://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz -OutFile llvm-6.0.0.src.tar.xz
+7z e llvm-6.0.0.src.tar.xz
+7z x llvm-6.0.0.src.tar
+mv llvm-6.0.0.src llvm
 
-git clone -q --depth 1 -b mesos_50 https://github.com/mesos/clang.git llvm/tools/clang
+git clone -q --depth 1 -b mesos_60 https://github.com/mesos/clang.git llvm/tools/clang
 
 cmake -G "Visual Studio 15 2017 Win64" -Thost=x64 -DCMAKE_INSTALL_PREFIX="${PREFIX}" llvm
 
